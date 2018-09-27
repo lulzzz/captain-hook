210ad949f27f730daa8e4d2ccb25a85726c194cd
David Rodrigues <davidferreirarodrigues@gmail.com>
2018-09-27 17:28:36 +0100

    Initial project template

---
+++ src/MessagingDirector.Interfaces/FabricTransport.cs
@@ -0,0 +1,3 @@
+﻿[assembly: Microsoft.ServiceFabric.Actors.Remoting.FabricTransport.FabricTransportActorRemotingProvider(
+    RemotingListenerVersion = Microsoft.ServiceFabric.Services.Remoting.RemotingListenerVersion.V2_1,
+    RemotingClientVersion = Microsoft.ServiceFabric.Services.Remoting.RemotingClientVersion.V2_1)]
