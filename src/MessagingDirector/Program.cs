ae3d7b4535d53cd58006113f9d6d307f4f73fcec
djfr <davidferreirarodrigues@gmail.com>
2018-10-01 14:38:47 +0100

    Added autofac bootstrap to all actors

--- src/MessagingDirector/Program.cs
+++ src/MessagingDirector/Program.cs
@@ -2,7 +2,8 @@
 {
     using System;
     using System.Threading;
-    using Microsoft.ServiceFabric.Actors.Runtime;
+    using Autofac;
+    using Autofac.Integration.ServiceFabric;
 
     internal static class Program
     {
@@ -13,15 +14,16 @@
         {
             try
             {
-                // This line registers an Actor Service to host your actor class with the Service Fabric runtime.
-                // The contents of your ServiceManifest.xml and ApplicationManifest.xml files
-                // are automatically populated when you build this project.
-                // For more information, see https://aka.ms/servicefabricactorsplatform
+                var builder = new ContainerBuilder();
+                builder.RegisterServiceFabricSupport();
 
-                ActorRuntime.RegisterActorAsync<MessagingDirector>(
-                   (context, actorType) => new ActorService(context, actorType)).GetAwaiter().GetResult();
+                builder.RegisterActor<MessagingDirector>();
 
-                Thread.Sleep(Timeout.Infinite);
+                using (builder.Build())
+                {
+                    // Prevents this host process from terminating so services keep running.
+                    Thread.Sleep(Timeout.Infinite);
+                }
             }
             catch (Exception e)
             {
