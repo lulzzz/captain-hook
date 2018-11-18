4323036cf49b92c2dd0855ca575ef7c663bf5796
David Rodrigues <davidferreirarodrigues@gmail.com>
2018-11-18 14:00:45 +0000

    Added resource management to  the pool manager actor

--- src/CaptainHook.PoolManagerActor/PoolManagerActor.cs
+++ src/CaptainHook.PoolManagerActor/PoolManagerActor.cs
@@ -1,10 +1,14 @@
 ﻿namespace CaptainHook.PoolManagerActor
 {
     using System;
+    using System.Collections.Generic;
     using System.Threading.Tasks;
+    using Eshopworld.Core;
     using Interfaces;
     using Microsoft.ServiceFabric.Actors;
+    using Microsoft.ServiceFabric.Actors.Client;
     using Microsoft.ServiceFabric.Actors.Runtime;
+    using Microsoft.ServiceFabric.Data;
 
     /// <remarks>
     /// This class represents an actor.
@@ -17,28 +21,46 @@
     [StatePersistence(StatePersistence.Persisted)]
     internal class PoolManagerActor : Actor, IPoolManagerActor
     {
+        private readonly IBigBrother _bigBrother;
+        private ConditionalValue<HashSet<int>> _free; // free pool resources
+        private ConditionalValue<HashSet<int>> _busy; // busy pool resources
+
+        private const int NumberOfHandlers = 10; // TODO: TWEAK THIS - HARDCODED FOR NOW
+
         /// <summary>
         /// Initializes a new instance of PoolManagerActor
         /// </summary>
         /// <param name="actorService">The Microsoft.ServiceFabric.Actors.Runtime.ActorService that will host this actor instance.</param>
         /// <param name="actorId">The Microsoft.ServiceFabric.Actors.ActorId for this actor instance.</param>
-        public PoolManagerActor(ActorService actorService, ActorId actorId)
+        /// <param name="bigBrother">The <see cref="IBigBrother"/> instanced used to publish telemetry.</param>
+        public PoolManagerActor(ActorService actorService, ActorId actorId, IBigBrother bigBrother)
             : base(actorService, actorId)
         {
+            _bigBrother = bigBrother;
         }
 
         /// <summary>
         /// This method is called whenever an actor is activated.
         /// An actor is activated the first time any of its methods are invoked.
         /// </summary>
-        protected override Task OnActivateAsync()
+        protected override async Task OnActivateAsync()
         {
-            // The StateManager is this actor's private state store.
-            // Data stored in the StateManager will be replicated for high-availability for actors that use volatile or persisted state storage.
-            // Any serializable object can be saved in the StateManager.
-            // For more information, see https://aka.ms/servicefabricactorsstateserialization
+            _free = await StateManager.TryGetStateAsync<HashSet<int>>("free");
 
-            return this.StateManager.TryAddStateAsync("count", 0);
+            if (_free.HasValue)
+            {
+                _busy = await StateManager.TryGetStateAsync<HashSet<int>>(nameof(_busy));
+            }
+            else
+            {
+                for (var i = 0; i < NumberOfHandlers; i++)
+                {
+                    ActorProxy.Create<IEventReaderActor>(new ActorId(i));
+                    _free.Value.Add(i);
+                }
+
+                await StateManager.AddOrUpdateStateAsync(nameof(_free), _free, (s, value) => _free);
+            }
         }
 
         public async Task<Guid> DoWork(string payload, string type)
