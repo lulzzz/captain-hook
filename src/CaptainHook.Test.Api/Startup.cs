3dbad5d91c67cf143a7bec3ebd499f369429f477
David Rodrigues <davidferreirarodrigues@gmail.com>
2019-02-11 09:03:04 +0000

    v1 re-alignment -> nc2.2 + actors bump + closer to final set of moving parts

--- src/CaptainHook.Test.Api/Startup.cs
+++ src/CaptainHook.Test.Api/Startup.cs
@@ -19,31 +19,31 @@ namespace CaptainHook.Test.Api
 
         public Startup(IHostingEnvironment env)
         {
-            //try
-            //{
-            //    var configuration = EswDevOpsSdk.BuildConfiguration(env.ContentRootPath, env.EnvironmentName);
-            //    configuration.GetSection("Telemetry").Bind(_telemetrySettings);
-            //    _bb = new BigBrother(_telemetrySettings.InstrumentationKey, _telemetrySettings.InternalKey);
-            //}
-            //catch (Exception e)
-            //{
-            //    BigBrother.Write(e);
-            //    throw;
-            //}
+            try
+            {
+                var configuration = EswDevOpsSdk.BuildConfiguration(env.ContentRootPath, env.EnvironmentName);
+                configuration.GetSection("Telemetry").Bind(_telemetrySettings);
+                _bb = new BigBrother(_telemetrySettings.InstrumentationKey, _telemetrySettings.InternalKey);
+            }
+            catch (Exception e)
+            {
+                BigBrother.Write(e);
+                throw;
+            }
         }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        public IServiceProvider ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
-            //var builder = new ContainerBuilder();
-            //builder.Populate(services);
-            //builder.RegisterInstance(_bb).As<IBigBrother>().SingleInstance();
-            //builder.RegisterInstance(_telemetrySettings).As<TelemetrySettings>().SingleInstance();
+            var builder = new ContainerBuilder();
+            builder.Populate(services);
+            builder.RegisterInstance(_bb).As<IBigBrother>().SingleInstance();
+            builder.RegisterInstance(_telemetrySettings).As<TelemetrySettings>().SingleInstance();
 
-            //var container = builder.Build();
-            //return new AutofacServiceProvider(container);
+            var container = builder.Build();
+            return new AutofacServiceProvider(container);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
