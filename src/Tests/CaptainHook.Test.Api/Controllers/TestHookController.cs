4e10719daf2b53f881267dd2df618397ba562855
David Guerin <david.guerin@developershelf.com>
2019-02-21 16:45:00 +0000

    More merge clean up and nuget updates

---
+++ src/Tests/CaptainHook.Test.Api/Controllers/TestHookController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CaptainHook.Test.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestHookController : ControllerBase
+    {
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut]
+        public void Put([FromBody] string value)
+        {
+        }
+    }
+}
---
+++ src/Tests/CaptainHook.Test.Api/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.ServiceFabric.Services.Runtime;
+using System;
+using System.Threading;
+using Eshopworld.Telemetry;
+
+namespace CaptainHook.Test.Api
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// This is the entry point of the service host process.
+        /// </summary>
+        private static void Main()
+        {
+            try
+            {
+                // The ServiceManifest.XML file defines one or more service type names.
+                // Registering a service maps a service type name to a .NET type.
+                // When Service Fabric creates an instance of this service type,
+                // an instance of the class is created in this host process.
+
+                ServiceRuntime.RegisterServiceAsync("CaptainHook.Test.ApiType",
+                    context => new WebApiService(context)).GetAwaiter().GetResult();
+
+                // Prevents this host process from terminating so services keeps running. 
+                Thread.Sleep(Timeout.Infinite);
+            }
+            catch (Exception ex)
+            {
+                BigBrother.Write(ex);
+                throw;
+            }
+        }
+    }
+}
