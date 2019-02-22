37036c9671ba488cc6cb71eb2d45675d8da91e84
David Guerin <david.guerin@developershelf.com>
2019-02-22 12:11:24 +0000

    simple controller changes

---
+++ src/CaptainHook.Api/Controllers/WebHookController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Net;
+using CaptainHook.Common.Configuration;
+using CaptainHook.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.ServiceFabric.Actors;
+using Microsoft.ServiceFabric.Actors.Client;
+
+namespace CaptainHook.Api.Controllers
+{
+    /// <summary>
+    /// The Webhook configuration
+    /// </summary>
+    [ApiVersion("1.0")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [Produces("application/json")]
+    [Authorize]
+    public class WebHookController : Controller
+    {
+        private readonly IMessagingDirector _messagingDirectorActor;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public WebHookController()
+        {
+            var serviceUri = new Uri("fabric:/CaptainHook/MessagingDirectorActor");
+            _messagingDirectorActor = ActorProxy.Create<IMessagingDirector>(new ActorId(0), serviceUri);
+        }
+
+        //todo get paginated list
+
+        /// <summary>
+        /// Get a webhook configuration
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [ProducesResponseType(typeof(WebhookConfig), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(string), (int)HttpStatusCode.NotFound)]
+        [ProducesResponseType((int)HttpStatusCode.Unauthorized)]
+        public IActionResult Get(string name)
+        {
+            return Ok(_messagingDirectorActor.ReadWebhook(name));
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="config"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [ProducesResponseType(typeof(WebhookConfig), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(string[]), (int)HttpStatusCode.BadRequest)]
+        [ProducesResponseType((int)HttpStatusCode.Unauthorized)]
+        public IActionResult Post(WebhookConfig config)
+        {
+            config = _messagingDirectorActor.CreateWebhook(config);
+            return Ok(config);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="config"></param>
+        /// <returns></returns>
+        [HttpPut]
+        [ProducesResponseType(typeof(string[]), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(string[]), (int)HttpStatusCode.BadRequest)]
+        [ProducesResponseType((int)HttpStatusCode.Unauthorized)]
+        public IActionResult Put(WebhookConfig config)
+        {
+            config = _messagingDirectorActor.UpdateWebhook(config);
+            return Ok(config);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        [HttpDelete]
+        [ProducesResponseType((int)HttpStatusCode.NoContent)]
+        [ProducesResponseType((int)HttpStatusCode.Unauthorized)]
+        public IActionResult Delete(string name)
+        {
+            _messagingDirectorActor.DeleteWebhook(name);
+            return NoContent();
+        }
+    }
+}
--- src/CaptainHook.Interfaces/IMessagingDirector.cs
+++ src/CaptainHook.Interfaces/IMessagingDirector.cs
@@ -1,14 +1,46 @@
-﻿namespace CaptainHook.Interfaces
-{
-    using System.Threading.Tasks;
-    using Microsoft.ServiceFabric.Actors;
+﻿using System.Threading.Tasks;
+using CaptainHook.Common.Configuration;
+using Microsoft.ServiceFabric.Actors;
 
+namespace CaptainHook.Interfaces
+{
     /// <summary>
     /// This interface defines the methods exposed by an actor.
     /// Clients use this interface to interact with the actor that implements it.
     /// </summary>
     public interface IMessagingDirector : IActor
     {
+        /// <summary>
+        /// Run the director
+        /// </summary>
+        /// <returns></returns>
         Task Run();
+
+        /// <summary>
+        /// Read an existing webhook config
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        WebhookConfig ReadWebhook(string name);
+
+        /// <summary>
+        /// Create a new webhook
+        /// </summary>
+        /// <param name="config"></param>
+        WebhookConfig CreateWebhook(WebhookConfig config);
+
+        /// <summary>
+        /// Update an existing webhook
+        /// //todo need to think about authorisation
+        /// </summary>
+        /// <param name="config"></param>
+        WebhookConfig UpdateWebhook(WebhookConfig config);
+
+        /// <summary>
+        /// Delete an existing webhook
+        /// /todo need to think about authorisation
+        /// </summary>
+        /// <param name="name">Name given to the webhook</param>
+        void DeleteWebhook(string name);
     }
 }
